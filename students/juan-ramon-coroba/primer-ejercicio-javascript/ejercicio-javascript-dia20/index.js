a279593339cad4951a35b4c13d1da5df4890ca27
Korobajr <jrcoroba@hotmail.com>
2022-01-20 21:57:06 +0100

    termino ejercicio semáforo

--- students/juan-ramon-coroba/primer-ejercicio-javascript/ejercicio-javascript-dia20/index.js
+++ students/juan-ramon-coroba/primer-ejercicio-javascript/ejercicio-javascript-dia20/index.js
@@ -1,5 +1,30 @@
-let semaforo = prompt ('Selecciona un color del semaforo')
+let semaforo = prompt('Escribe un color del semaforo')
 
-switch (semaforo){
-    case class
+document.write('<div class="semaforo">');
+
+switch (semaforo) {
+    case 'rojo': {
+        document.write('<div class="semaforo__circulo rojo"></div>')
+        document.write('<div class="semaforo__circulo"></div>')
+        document.write('<div class="semaforo__circulo"></div>')
+
+        break;        
+    }
+    case 'amarillo': {
+        document.write('<div class="semaforo__circulo"></div>')
+        document.write('<div class="semaforo__circulo amarillo"></div>')
+        document.write('<div class="semaforo__circulo"></div>')
+
+        break;
+    }
+    case 'verde': {
+        document.write('<div class="semaforo__circulo"></div>')
+        document.write('<div class="semaforo__circulo"></div>')
+        document.write('<div class="semaforo__circulo verde"></div>')
+
+        break;
+    }
+        
 }
+document.write('</div>');
+
