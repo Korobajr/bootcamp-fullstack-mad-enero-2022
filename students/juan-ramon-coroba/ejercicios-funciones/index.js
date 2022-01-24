37a87fa8b44161096f384820c3a557df90e8f18e
USUARIO-PC\USUARIO <jrcoroba@hotmail.com>
2022-01-24 18:09:05 +0100

    ejercicios funciones días 24

---
+++ students/juan-ramon-coroba/ejercicios-funciones/index.js
@@ -0,0 +1,55 @@
+/*function resta(a,b){
+    return a-b;
+}
+
+const result = resta(4,2)
+document.write(`<p> ${result} </p>`)
+
+const result2 = resta (10,3)
+document.write(`<p> ${result2} </p>`)
+
+const result3 = resta (10,7)
+document.write(`<p> ${result3} </p>`)
+
+
+function lenght(str){
+    return str.lenght%2===0;
+}
+
+let userString= prompt('Introduzca una frase o palabra');
+document.write(`<p>${lenght(userString)}</p>`);
+userString= prompt('Introduzca una frase o palabra');
+document.write(`<p>${lenght(userString)}</p>`);
+
+const paramType = a => typeof a;
+
+let userInput= true
+document.write(paramType(userInput))
+
+
+const a= prompt('Introduce un true o un false')
+const b= prompt('Introduce un true o un false')
+
+function bool(a,b){
+    let n= a=== 'True';
+    let m= b=== 'True';
+    return n||m
+}
+
+document.write(bool(a,b))
+
+
+const num= prompt('Introduce un número de dos dígitos o más')
+
+function reves(a){
+    let numReverse= "";
+    for (let i=a.length; i>=0; i--){
+        numReverse+=a.charAt(i);
+    }
+    return numReverse;
+}
+
+document.write(reves(num))
+*/
+
+
