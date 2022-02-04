8a2e0abdbcb19fa65f0a0ce79a7743b570042a72
USUARIO-PC\USUARIO <jrcoroba@hotmail.com>
2022-02-04 08:48:44 +0100

    ejercicios dia 03 del 02

--- students/juan-ramon-coroba/ejercicios-dia-03-02/index.js
+++ students/juan-ramon-coroba/ejercicios-dia-03-02/index.js
@@ -4,17 +4,17 @@
 
 const obj1 = {
     description: 'Jose',
-    votes: 9
+    votes: 0
 }
 
 const obj2 = {
     description: 'Paco',
-    votes: 5
+    votes: 0
 }
 
 const obj3 = {
     description: 'Chema',
-    votes: 10
+    votes: 0
 }
 const arr = [];
 arr.push(obj1,obj2,obj3);
@@ -24,22 +24,46 @@ arr.push(obj1,obj2,obj3);
 let Ul = document.createElement('ul')
 document.body.appendChild(Ul)
 
-function createList (arr){ // funcion que cuando sea invocada cree tantas estructuras html como elementos tenga el array
-    arr.forEach(element => { // recorro el parametro array, cada elemento del array, es decir cada objeto
+function createList (element){ // funcion que cuando sea invocada cree tantas estructuras html como elementos tenga el array
+     // recorro el parametro array, cada elemento del array, es decir cada objeto
         let liNew = document.createElement('li') // variable que crea un li html
         liNew.textContent= element.description // cada li creado se rellena con el valor, description de cada elemento(onjeto)
         let buttonVote = document.createElement('button') //variable que crea un boton html
         buttonVote.textContent = element.votes // el boton creado se rellena con el valor votes del objeto (elemento)
         Ul.appendChild(liNew) //inserto el li creado en js hacia Html
         Ul.appendChild(buttonVote) // inserto el boton en el html
-    });
+        let cont= 0; // inicio una variable contador a 0
+        buttonVote.addEventListener('click', () =>{ // añado un escuchador al boton de contador de votos
+            cont++; // cada activacion se añade 1 y se suma a la variable cont
+            buttonVote.textContent= cont // igualo la variable cont al valor de texto del boton que cuenta los votos
+        })
 }
 
 createList(arr)
+//crear el input y el botn en html
+//traerme los dos elementos
 
+arr.forEach(element => {
+    createList(element);
+});
+
+const btnone = document.querySelector('.buttonClass');
+const input = document.querySelector('.inputClass');
+
+btnone.addEventListener('click', ()=>{ 
+    const mari = {
+        description: input.ariaValueMax,
+        vote: 0
+    };
+    createList(mari)
+    arr.push(mari)
+} )
+/*
+const buttonInput = document.createElement('button');
+document.body.appendChild(buttonInput)
+buttonInput.addEventListener('click', () =>{
+    document.createElement('object')
+})*/
 // Cuando se pulse el botón, añadir uno a las votaciones actualizando el HTML
-
-function actVote (){
-    buttonVote.add
-}
-
+// Añadir un input y un botón para poder crear un nuevo objeto para votar. En este punto podemos vaciar el array 
+// de objetos inicial, ya que los temas de votación los introduciremos desde la interfaz
